7d74aaa75ef5bf0b66f85ef9e976527386471097
Felix <263016940+Felix-HC@users.noreply.github.com>
2026-02-22 00:29:35 +0100

    feat: add app title

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,13 @@ import './App.css'
 
 function App() {
   return (
-    <div>
-      <p>Hello World!</p>
+    <div id="app">
+      <header>
+        <div id="app-title">
+          <p id="app-title-surface">Flavorstats</p>
+          <div id="app-title-back" />
+        </div>
+      </header>
     </div>
   )
 }
