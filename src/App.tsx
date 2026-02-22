6bba66e043838a8fbbd361e3ea86a7d377385e62
Felix <263016940+Felix-HC@users.noreply.github.com>
2026-02-22 01:15:06 +0100

    feat: basic search functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,22 @@
+import { useState } from 'react'
+
 import './App.css'
 
 function App() {
+  const [results, setResults] = useState<Array<any> | undefined>(undefined);
+  const [searchInput, setSearchInput] = useState<string>("");
+  const [showingResults, showResults] = useState<boolean>(false);
+
+  const searchUsers = () => {
+    console.log(`http://localhost:5000/search?query=${searchInput}`);
+
+    fetch(`http://localhost:5000/search?query=${searchInput}`)
+      .then(response => response.json())
+      .then(data => setResults(data));
+
+    showResults(true);
+  }
+
   return (
     <div id="app">
       <header>
@@ -9,6 +25,28 @@ function App() {
           <div id="app-title-back" />
         </div>
       </header>
+      <main>
+        <div id="input-wrapper">
+          <h2>Search Users</h2>
+          <form onSubmit={(e) => { e.preventDefault(); searchUsers() }}>
+            <input id="user-input" type="text" placeholder="_" onChange={(e) => setSearchInput(e.target.value)} />
+          </form>
+        </div>
+        {showingResults &&
+          <div id="search-results">
+            <li>
+              {results?.map((user, index) => {
+                return (
+                  <ul key={index}>
+                    <img src={user.avatar} />
+                    <span>{user.display_name}</span>
+                  </ul>
+                )
+              })}
+            </li>
+          </div>
+        }
+      </main>
     </div>
   )
 }
